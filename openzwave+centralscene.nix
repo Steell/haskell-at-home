bae380b162674a70e969a0fad8136f0bdced7bcb
Steve Elliott <steve@steellworks.com>
2018-11-23 15:19:21 -0500

    Revert "update ozw to dev"
    This reverts commit 175d0891da31053de7380d65471e60db51ba156c.

--- openzwave+centralscene.nix
+++ openzwave+centralscene.nix
@@ -6,7 +6,7 @@ openzwave.overrideAttrs (oldAttrs: {
   src=fetchFromGitHub {
     owner = "OpenZWave";
     repo = "open-zwave";
-    rev = "52a8451ad4676b157d081a77182c8496e4d20e99";
-    sha256 = "145ywkzg4yj8rm27bi8n5y6b8jy7k7cvlicj4p3ha0v01v0nai2r";
+    rev = "1099fa729e28c863c311837bf4da852b8d2a80e9";
+    sha256 = "0wgc1wh0jppkmx585s83im4m7nv7q8fxnyia8cnjqkk8hxlp3yd8";
   };
 })
